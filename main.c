953a0ddb76e6673237b217dac17891e31bb9e4a9
lilly5791 <1913084@sookmyung.ac.kr>
2019-10-31 15:44:01 +0900

    09silsup4

--- main.c
+++ main.c
@@ -1,24 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define SIZE 5
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+void square_array(int a[], int size);
+void print_array(int a[], int size);
 
 int main(int argc, char *argv[]) {
 	
-	int a[SIZE] = {1, 2, 3, 4, 5};
-	int b[SIZE] = {1, 2, 3, 4, 0};
+	int list[4] = {1, 2, 3, 4};
 	int i;
 
-	
-	for(i = 0; i < SIZE; i++)
-	{
-		if(a[i]!=b[i])
-			printf("%d is different\n", i);		
-	}
-		
-	
+	print_array(list, 4);
+	square_array(list, 4);
+	print_array(list, 4);
 
 	return 0;
 }
+
+void square_array(int a[], int size)
+{
+	int i;
+	for(i = 0; i < 4; i++)
+		a[i] = a[i] * a[i];
+}
+
+void print_array(int a[], int size)
+{
+	int i;
+	for(i = 0; i < 4; i++)
+	{
+		printf("%d\n", a[i]);
+	}
+}
+
