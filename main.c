c5a7eb7b43ec9eb08af26fedc2a3134d6ec69489
lilly5791 <1913084@sookmyung.ac.kr>
2019-10-31 15:16:22 +0900

    09silsup2

--- main.c
+++ main.c
@@ -1,21 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#define SIZE 5
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
 	
-	int student[5];
-	int i, j;
+	int student[SIZE];
+	int i, sum = 0;
 	
-	student[0] = 10;
-	student[1] = 20;
-	student[2] = 30;
-	student[3] = 40;
-	student[4] = 50;
-	
-	for(i = 0; i < 5; i++)
-		printf("%d\n", student[i]);
-	
+	for(i = 0; i < SIZE; i++)
+	{
+		printf("input grade[%d]:", i);
+		scanf("%d", &student[i]);
+	}
+		
+	for(i = 0; i < SIZE; i++)
+	{
+		sum += student[i];
+		printf("student[%d] = %d\n", i, student[i]);
+	}
+	printf("average is %d\n", sum/5);
+
 	return 0;
 }
